45f98f82700b754103b24244118d5da221607120
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-05 16:23:43 -0300

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+  ,
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
