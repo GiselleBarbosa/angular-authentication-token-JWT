6968ba33a2a2509862f17d24f22b5946e177ad28
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-07 19:29:45 -0300

    adicionada logica para ocultar icones no header caso usuario logado

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  private userService = inject(UserService);
+  private router = inject(Router);
 
+  public user$ = this.userService.user$;
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
