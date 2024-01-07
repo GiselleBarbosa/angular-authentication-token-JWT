7e387a163f260cdb079515e26a27b48217879010
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-07 18:38:12 -0300

    dados do usuario estao sendo recuperados ao acessar componente

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,20 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+
+import { CadastroService } from 'src/app/core/services/cadastro.service';
+import { FormGroup } from '@angular/forms';
+import { FormularioService } from 'src/app/core/services/formulario.service';
+import { PessoaUsuaria } from 'src/app/core/types/type';
+import { TokenService } from './../../core/services/token.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss'],
 })
-export class PerfilComponent {
-  public titulo = 'Olá, usuário!';
+export class PerfilComponent implements OnInit {
+  private tokenService = inject(TokenService);
+  private cadastroService = inject(CadastroService);
+  private formularioService = inject(FormularioService);
+
+  public titulo = 'Olá, ';
   public textoBotao = 'ATUALIZAR';
   public perfilComponent = true;
 
-  deslogar() {
+  public token = '';
+  public nome = '';
+  public cadastro!: PessoaUsuaria;
+  form!: FormGroup | null;
+
+  public ngOnInit(): void {
+    this.token = this.tokenService.retornarToken();
+    this.cadastroService.buscarCadastro(this.token).subscribe((cadastro) => {
+      this.cadastro = cadastro;
+      this.nome = this.cadastro.nome;
+      this.carregarFormulario();
+    });
+  }
+
+  public carregarFormulario() {
+    this.form = this.formularioService.getCadastro();
+    this.form?.patchValue({
+      nome: this.cadastro.nome,
+      nascimento: this.cadastro.nascimento,
+      cpf: this.cadastro.cpf,
+      telefone: this.cadastro.telefone,
+      email: this.cadastro.email,
+      senha: this.cadastro.senha,
+      genero: this.cadastro.genero,
+      cidade: this.cadastro.cidade,
+      estado: this.cadastro.estado,
+    });
+  }
+
+  public deslogar() {
     console.log('deslogar');
   }
 
-  atualizar() {
+  public atualizar() {
     console.log('atualizar');
   }
 }
