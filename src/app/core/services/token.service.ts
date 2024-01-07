d4d6d87613127fdb8115f542a286be86fdb56cf5
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-07 13:41:37 -0300

    criado service para gerenciar token

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  public salvarToken(token: string): void {
+    return localStorage.setItem(KEY, token);
+  }
+
+  public excluirToken(): void {
+    localStorage.removeItem(KEY);
+  }
+
+  public retornarToken(): string | null {
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  public possuiToken(): boolean {
+    return !!this.retornarToken();
+  }
+}
