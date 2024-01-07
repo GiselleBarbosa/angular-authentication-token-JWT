b1c189717eff63599817be59d7b92716913683c1
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-07 10:43:27 -0300

    criado formulario de cadastro e feitas algumas mudancas no estilo do dropdown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BannerComponent } from './shared/banner/banner.component';
-import { CardComponent } from './shared/card/card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { ContainerComponent } from './shared/container/container.component';
-import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
-import { MatCardModule } from '@angular/material/card';
-import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
-import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { ModalComponent } from './shared/modal/modal.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-
 import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { CardComponent } from './shared/card/card.component';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+import { ContainerComponent } from './shared/container/container.component';
+import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
+import { DropdownUfComponent } from './shared/dropdown-uf/dropdown-uf.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FormBaseComponent } from './shared/form-base/form-base.component';
+import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ModalComponent } from './shared/modal/modal.component';
+import { NgModule } from '@angular/core';
 import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DropdownUfComponent } from './shared/form-busca/dropdown-uf/dropdown-uf.component';
 import { SeletorPassageiroComponent } from './shared/seletor-passageiro/seletor-passageiro.component';
-import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
-import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -54,7 +56,8 @@ import { LoginComponent } from './pages/login/login.component';
     DropdownUfComponent,
     SeletorPassageiroComponent,
     DepoimentosComponent,
-    LoginComponent
+    LoginComponent,
+    FormBaseComponent,
   ],
   imports: [
     BrowserModule,
@@ -73,9 +76,12 @@ import { LoginComponent } from './pages/login/login.component';
     MatDialogModule,
     HttpClientModule,
     ReactiveFormsModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatDividerModule,
+    MatCheckboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
